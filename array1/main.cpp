9bec03e885db408f8376c5ef5a06c9470e77c12e
adrialva <58445399+adrialva@users.noreply.github.com>
2021-06-24 06:52:13 -0500

    Add files via upload

---
+++ array1/main.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+char arreglo2[6][6];
+
+void lleno (){
+	for (int f=0;f<6; f++){
+		for(int c=0; c<6;c++){
+			arreglo2 [f][c]='A';
+		}
+	}
+}
+	void segundo (){
+		for( int f=0; f<6; f++){
+			for (int c=0; c<6; c++){
+				cout <<arreglo2[f][c]<<" ";
+			}
+			cout <<"\n";
+		}
+	}
+int main (int argc, char *argv[]) {
+	lleno();
+	segundo();
+	return 0;
+}
+
