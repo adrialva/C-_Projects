68fc410992def72ce0def856d644daa348560e7a
adrialva <58445399+adrialva@users.noreply.github.com>
2021-06-28 17:08:31 -0500

    Add files via upload

---
+++ TareaB.cpp/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	int d=0;
+	int c=1;
+	int n=0;
+	int b=0;
+	cout<< "Pedir numero";
+	cin >>n;
+	while (b<c){
+		cout <<"A";
+		b=b+1;
+		cout <<"\n";
+	}
+	if(c<n){
+		b=0;
+		c=c+1;
+	}
+	return 0;
+	}
+}
+
+
