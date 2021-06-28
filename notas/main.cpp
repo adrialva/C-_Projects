a990cfb17c3bdad9c49cd3190772a91242090f05
adrialva <58445399+adrialva@users.noreply.github.com>
2021-06-28 17:01:56 -0500

    Add files via upload

---
+++ notas/main.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	int n=0;
+	cout <<"Ingrese la nota";
+	cin >>n;
+	if (n>10.0){
+		cout <<"ERROR";
+	}
+	else {
+		if(n>=9.5);
+		cout <<"Matricula de honor";
+		return 0;
+		if (8.5 <= n <9.5){
+		cout <<"Sobresaliente";
+		if (6.5<=n<8.5){
+	cout <<"Notable";
+	}
+	if (5<=n<6.5){
+		cout <<"Aprobado";
+	}
+	if (n<5){
+		cout<<"Reprobado";
+	}
+	}
+	}
+}
+
+
+
