da07983583c2eeb187fea2b4230cf6371b1eb606
adrialva <58445399+adrialva@users.noreply.github.com>
2021-06-28 17:00:18 -0500

    Add files via upload

---
+++ multiplicacion.cpp/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	int b=1000;
+	int c=0;
+	int a=0;
+	int d=0;
+	cout <<"Escriba numero que desa multiplicar";
+	cin >> a;
+	while (c!=b){
+		d=a*c;
+		cout << a"x"c"="d;
+		c=c+1;
+	}
+	if (c==b){
+		cout <<d;
+	}
+	return 0;
+}
+
