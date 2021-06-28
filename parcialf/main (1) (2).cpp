6c783baa2a4b1061074c31cbbff882c67f7fec73
adrialva <58445399+adrialva@users.noreply.github.com>
2021-06-28 17:03:54 -0500

    Add files via upload

---
+++ parcialf/main (1) (2).cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	int c=0;
+	int b=0;
+	int n=0;
+	int d=0;
+	int s=0;
+	int p=0;
+	cout<<"pedir numero ";
+	cin >>n;
+	b=n-1;
+	s=0;
+	while (d<n){
+
+		while (c<b){
+			cout <<" ";
+			c=c+1;
+		}
+		c=0;
+		d=d+1;
+		b=b-1;
+		while(p<=s){
+			p=p+1;
+			cout<<"*";
+		}
+		p=0;
+		s=s+1;
+		cout<<"\n";
+	}
+	return 0;
+}
+
---
+++ parcialf/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	int n=0;//numero dado
+	int a=0;//contador final restador
+	int b=0;//acont
+	int c=0;//bccont
+	int d=0;//ccont
+	cout <<" Digite un numero ";
+	cin>>n;
+	a=n;
+	while (c<=n){
+		b=0;
+		c=0;
+		while(b<a){
+			cout <<" ";
+			b=b+1;
+		}
+		while(c<=d){
+			cout <<"*"<<" ";
+			c=c+1;
+		}
+		cout<< "\n";
+		d=d+1;
+		a=a-1;
+	}
+	return 0;
+}
