464dffc6f85131fbea7dc28553a03e2381155a6b
adrialva <58445399+adrialva@users.noreply.github.com>
2021-06-28 17:01:26 -0500

    Add files via upload

---
+++ multiplicacionextensa.cpp/main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	int a=2;
+	int b=3;
+	int d=0;
+	int c=0;
+	cout<<a;
+	cout <<b;
+	if (b==0){
+		cout<<"el resultado es 0";
+	}
+	else{
+		(b!=0);
+		if(b==c){
+			cout <<d;
+		}
+		while (b!=c){
+			c=c+1;
+			cout <<d;
+			d=d+a;
+		}
+	}
+	
+	return 0;
+}
+
