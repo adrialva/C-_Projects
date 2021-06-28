edd60eb0d10a795cbdce2de14faa9d7142156524
adrialva <58445399+adrialva@users.noreply.github.com>
2021-06-28 17:08:07 -0500

    Add files via upload

---
+++ tareaA.cpp/main.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	int d=0;
+	int c=0;
+	int n=0;
+	int b=0;
+	cout<< "pedir numero ";
+	cin>>n;
+	b=n;
+	while (d!=n){
+		while (c<=b){
+			if (c>b){
+				c=c+1;
+			}
+			else{
+				(c!=b);
+				cout <<"A";
+				c=c+1;
+			}
+		}
+	
+		cout <<"\n";
+		c=0;
+		b=b-1;
+		d=d+1;
+	}
+		cout <<"A";
+}
+	
+
+
